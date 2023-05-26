0d2e19b40114c51202118cc5f90df4eb988f2d24
Charlie <gamerboy030607@gmail.com>
2023-05-26 17:20:26 +1000

    W

--- python/main.py
+++ python/main.py
@@ -4,7 +4,7 @@ import time
 import subprocess
 import socket
 
-from python.st import pydata1,pydata2,pydata3,pydata4,pydata5,pydata6,pydata7,pydata8,pydata9,pydata10
+from st import pydata1,pydata2,pydata3,pydata4,pydata5,pydata6,pydata7,pydata8,pydata9,pydata10
 import platform
 # def main():
 #     if platform.system == 'windows':
